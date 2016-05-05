78250bd9aafccfa96cda3f83c49034e631ec0cf1
LaryIV <zhouhai_justdoit@qq.com>
2016-05-06 01:02:08 +0800

    tabbar

--- niuyou/index.ios.js
+++ niuyou/index.ios.js
@@ -1,53 +1,161 @@
-/**
- * niuyou React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// /**
+//  * niuyou React Native App
+//  * https://github.com/facebook/react-native
+//  * @flow
+//  */
+//
+// import React, { Component } from 'react';
+// import {
+//   AppRegistry,
+//   StyleSheet,
+//   Text,
+//   View
+// } from 'react-native';
+//
+// class niuyou extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit index.ios.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           Press Cmd+R to reload,{'\n'}
+//           Cmd+D or shake for dev menu
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+//
+// AppRegistry.registerComponent('niuyou', () => niuyou);
+'use strict';
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  TabBarIOS,
   Text,
-  View
+  View,
+  Image,
 } from 'react-native';
 
-class niuyou extends Component {
-  render() {
+var niuyou = React.createClass({
+  statics: {
+    title: '<TabBarIOS>',
+    description: 'Tab-based navigation.',
+  },
+
+  displayName: 'TabBarExample',
+
+  getInitialState: function() {
+    return {
+      selectedTab: 0,
+      notifCount: 0,
+      presses: 0,
+    };
+  },
+
+  _renderContent: function(color: string, pageText: string, num?: number) {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={[styles.tabContent, {backgroundColor: color}]}>
+        <Text style={styles.tabText}>{pageText}</Text>
+        <Text style={styles.tabText}>{num} re-renders of the {pageText}</Text>
       </View>
     );
-  }
-}
+  },
 
-const styles = StyleSheet.create({
-  container: {
+  render: function() {
+    return (
+      <TabBarIOS
+        tintColor="#51a7ff"
+        barTintColor="white">
+        <TabBarIOS.Item
+          icon={require('image!home_nor')}
+          title="主页"
+          selected={this.state.selectedTab === 0}
+          style={styles.tabbarItem}
+          onPress={() => {
+            this.setState({
+              selectedTab: 0,
+            });
+          }}>
+          {this._renderContent('#414A8C', 'Blue Tab')}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          icon={require('image!jrw_nor')}
+          title="接任务"
+          selected={this.state.selectedTab === 1}
+          onPress={() => {
+            this.setState({
+              selectedTab: 1,
+            });
+          }}>
+          {this._renderContent('#414A8C', 'Blue Tab')}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          icon={require('image!phb_nor')}
+          title="排行榜"
+          selected={this.state.selectedTab === 2}
+          onPress={() => {
+            this.setState({
+              selectedTab: 2,
+            });
+          }}>
+          {this._renderContent('#783E33', 'Red Tab')}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          icon={require('image!wo_nor')}
+          title="我的"
+          selected={this.state.selectedTab === 3}
+          onPress={() => {
+            this.setState({
+              selectedTab: 3,
+            });
+          }}>
+          {this._renderContent('#21551C', 'Green Tab', this.state.presses)}
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    );
+  },
+
+});
+
+var styles = StyleSheet.create({
+  tabContent: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  tabText: {
+    color: 'white',
+    margin: 50,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  tabbarItem:{
+    marginTop:-2
+  }
 });
 
 AppRegistry.registerComponent('niuyou', () => niuyou);
